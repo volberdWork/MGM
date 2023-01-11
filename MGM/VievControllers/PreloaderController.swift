02ad8b3ec44763aa358c50c9a24a81b4ec6c119c
VolberdWork <volodymyr.kolomyltsvev@businessgroup.ltd>
2023-01-11 17:10:15 +0200

    added pageView

--- MGM/VievControllers/PreloaderController.swift
+++ MGM/VievControllers/PreloaderController.swift
@@ -4,33 +4,47 @@ import SnapKit
 class PreloadreController: UIViewController {
     
     let logoImage = UIImage(named: "MGMLogo")
-
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
+       configureView()
         
- 
-        configureView()
         
     }
     
-   private func configureView(){
+    override func viewWillAppear(_ animated: Bool) {
        
+    }
+    
+    private func configureView(){
         view.backgroundColor = UIColor(displayP3Red: 41/255, green: 42/255, blue: 46/255, alpha: 1)
         let logo = UIImageView(image: logoImage)
         view.addSubview(logo)
-       logo.snp.makeConstraints { make in
-           make.center.equalToSuperview()
-           make.width.equalTo(145)
-           make.height.equalTo(45)
-       }
+        logo.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.width.equalTo(145)
+            make.height.equalTo(45)
+        }
+        let button = UIButton()
+        view.addSubview(button)
+        button.snp.makeConstraints { make in
+            make.height.equalTo(100)
+            make.width.equalTo(100)
+            make.center.equalToSuperview()
+        }
+        button.backgroundColor = .blue
+        button.addTarget(self, action: #selector(press), for: .touchUpInside)
         
         
-        
+       
     }
-    
-    
-
-
+    @objc func press(){
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        if let vc = main.instantiateViewController(withIdentifier: "PresentViewController") as? PresentViewController {
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+  
 }
 
---
+++ MGM/VievControllers/PresentViewController.swift
@@ -0,0 +1,74 @@
+import UIKit
+import SnapKit
+class PresentViewController: UIViewController{
+    
+    lazy var firstView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        return view
+    }()
+    
+    lazy var secondView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .yellow
+        return view
+    }()
+    
+    lazy var thirdView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemYellow
+        
+        return view
+    }()
+    
+    lazy var views = [firstView, secondView, thirdView]
+    
+    lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.isPagingEnabled = true
+        scrollView.contentSize = CGSize(width: view.frame.width * CGFloat(views.count), height: view.frame.height)
+        
+        for i in 0..<views.count {
+            scrollView.addSubview(views[i])
+            views[i].frame = CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width, height: view.frame.height)
+        }
+        
+        scrollView.delegate = self
+        
+        return scrollView
+    }()
+    
+    lazy var pageControl: UIPageControl = {
+        let pageControl = UIPageControl()
+        pageControl.numberOfPages = views.count
+        pageControl.currentPage = 0
+        pageControl.addTarget(self, action: #selector(pageControlTapHandler(sender:)), for: .touchUpInside)
+        return pageControl
+    }()
+    
+    @objc
+    func pageControlTapHandler(sender: UIPageControl) {
+        scrollView.scrollTo(horizontalPage: sender.currentPage, animated: true)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.addSubview(scrollView)
+        scrollView.edgeTo(view: view)
+        
+        view.addSubview(pageControl)
+        pageControl.pinTo(view)
+    }
+
+
+}
+
+extension PresentViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let pageIndex = round(scrollView.contentOffset.x / view.frame.width)
+        pageControl.currentPage = Int(pageIndex)
+    }
+}
