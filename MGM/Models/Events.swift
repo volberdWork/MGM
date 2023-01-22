1c5fce108b530bac95c056503508829af1746df8
VolberdWork <volodymyr.kolomyltsvev@businessgroup.ltd>
2023-01-23 00:39:03 +0200

    Events Models

---
+++ MGM/Models/Events.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+struct EventsBase : Codable {
+    let get : String?
+    let parameters : Parameters?
+    let errors : [String]?
+    let results : Int?
+    let response : [Response]?
+}
+
+struct EventsParameters : Codable {
+    let id : String?
+}
+
+struct EventsPlayer : Codable {
+    let id : Int?
+    let name : String?
+    let image : String?
+}
+
+struct EventsResponse : Codable {
+    let quarter : String?
+    let minute : String?
+    let team : EventsTeam?
+    let player : EventsPlayer?
+    let type : String?
+    let comment : String?
+    let score : EventsScore?
+}
+
+struct EventsScore : Codable {
+    let home : Int?
+    let away : Int?
+}
+
+struct EventsTeam : Codable {
+    let id : Int?
+    let name : String?
+    let logo : String?
+}
