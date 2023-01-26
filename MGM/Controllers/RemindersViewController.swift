373ccefbf642993704cef82a766a158eb6ce1bf0
VolberdWork <volodymyr.kolomyltsvev@businessgroup.ltd>
2023-01-26 13:56:29 +0200

    logic for save and collectionCell to reminders

--- MGM/Controllers/RemindersViewController.swift
+++ MGM/Controllers/RemindersViewController.swift
@@ -1,7 +1,8 @@
 import UIKit
 import RealmSwift
-
-class RemindersViewController: UIViewController{ @IBOutlet var collectionView: UICollectionView!
+class RemindersViewController: UIViewController{
+    
+    @IBOutlet var collectionView: UICollectionView!
     let realm = try? Realm()
     var realmArray: [InfoBaseRealm] = []
     var awaylogoLink = ""
@@ -12,8 +13,20 @@ class RemindersViewController: UIViewController{ @IBOutlet var collectionView: U
         setupView()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.realmArray = []
+        guard let infoMatchesResult = realm?.objects(InfoBaseRealm.self) else {return}
+        for match in infoMatchesResult{
+            self.realmArray.append(match)
+        }
+        self.collectionView.reloadData()
+        
+    }
+    
     func setupView(){
         self.collectionView.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0)
+       
     }
 }
 
@@ -25,11 +38,27 @@ extension RemindersViewController:UICollectionViewDelegate{
 
 extension RemindersViewController:UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return realmArray.count
+        return 10
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return UICollectionViewCell()
+       
+        let infoCell = collectionView.dequeueReusableCell(withReuseIdentifier: "RemindersCollectionViewCell", for: indexPath) as! RemindersCollectionViewCell
+        infoCell.layer.borderWidth = 0.4
+        infoCell.layer.borderColor = CGColor(red: 243/255, green: 244/255, blue: 245/255, alpha: 0.5)
+        return infoCell
+        
+       
     }
     
 }
+
+
+extension RemindersViewController: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return  CGSize(width: 347, height: 122)
+        
+    }
+}
+
---
+++ MGM/Views/Cells/RemindersCollectionViewCell.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+class RemindersCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var remindersButton: UIButton!
+    
+    @IBOutlet var timaDateLabel: UILabel!
+    
+    @IBOutlet var homeLogoImage: UIImageView!
+    
+    @IBOutlet var awayLogoImage: UIImageView!
+    
+    @IBOutlet var homeNameLabel: UILabel!
+    
+    @IBOutlet var awayNameLabel: UILabel!
+    
+    @IBOutlet var dateLabel: UILabel!
+    
+    
+    
+    
+    @IBAction func remindersButtonPressed(_ sender: UIButton) {
+    }
+}
