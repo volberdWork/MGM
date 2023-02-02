f8cf4416570d6f0e604ab238a64aa7da397fee96
VolberdWork <volodymyr.kolomyltsvev@businessgroup.ltd>
2023-02-02 09:20:13 +0200

    teamPlayerScreen

--- MGM/Controllers/DetailTeamViewController.swift
+++ MGM/Controllers/DetailTeamViewController.swift
@@ -1,12 +1,51 @@
 import UIKit
+import RealmSwift
 
 class DetailTeamViewController: UIViewController {
-
+    let realm = try? Realm()
     override func viewDidLoad() {
         super.viewDidLoad()
 
  
     }
     
-
+    
+    func saveToRealm(){
+        
+        let infoBaseRealm = InfoBaseRealm()
+        
+        try? self.realm?.write{
+            self.realm?.add(infoBaseRealm, update: .all)
+        }
+    }
+    
+    
+    func loadAllert(){
+        let alert = UIAlertController(title: "Save Event", message: "For save tap ok", preferredStyle: .alert)
+        
+        let ok = UIAlertAction(title: "OK", style: .default, handler: { action in
+            self.saveToRealm()
+//            self.saveButton.setImage(UIImage(named: "savedStar"), for: .normal)
+            
+            UIDevice.onOffVibration()
+        })
+        alert.addAction(ok)
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .default, handler: { action in
+            
+        })
+        alert.addAction(cancel)
+        DispatchQueue.main.async(execute: {
+            UIDevice.onOffVibration()
+            self.present(alert, animated: true)
+            
+        })
+    }
+    
+    
+    
+    
+    @IBAction func saveButtonPressed(_ sender: UIButton) {
+    }
+    
 }
